af734b854dba0af4b81385fe8a6a61d2eda06c4d
Areeb-Ali <areebithink@gmail.com>
2024-12-12 23:01:18 +0500

    Milestone 1 Completed

---
+++ Milestone1-2/script.js
@@ -0,0 +1,10 @@
+var toggelButton = document.getElementById('button');
+var skills = document.getElementById('skill');
+toggelButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
